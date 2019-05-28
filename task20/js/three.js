38685b66f0c2aa2e4a953109dfbb946790b4b553
admin <you@example.com>
2019-05-28 16:25:41 +0530

    commited

---
+++ task20/js/three.js
@@ -0,0 +1,29 @@
+debugger;
+function myfunction(){
+if(document.getElementById("p1").style.background =="blue"){
+	document.getElementById("p1").style.background ="white";
+
+	}
+	else{
+	document.getElementById("p1").style.background ="blue";
+}
+}
+function color(){
+if(document.getElementById("p2").style.background =="green"){
+	document.getElementById("p2").style.background ="white";
+
+	}
+	else{
+	document.getElementById("p2").style.background ="green";
+}
+}
+function text(){
+if(document.getElementById("p3").style.background =="red"){
+	document.getElementById("p3").style.background ="white";
+
+	}
+	else{
+	document.getElementById("p3").style.background ="red";
+}
+}
+
